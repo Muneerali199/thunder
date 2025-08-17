8ec94b3cdfee9e4fbea9348ff327cbeb26002764
Muneer Ali <alimuneerali245@gmail.com>
2025-08-18 04:15:19 +0530

    Update config.ts

--- thunder/frontend/src/config.ts
+++ thunder/frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://thunder-1-swdc.onrender.com/";
+export const BACKEND_URL = "https://thunder-1-swdc.onrender.com";
 
 //https://website-builder-backend-ws9k.onrender.com
